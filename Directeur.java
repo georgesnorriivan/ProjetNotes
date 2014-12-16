73e9b1361b416fe502246d9d67b0f8791ada1918
Meven LE GOFF <meven.legoff@hotmail.fr>
2014-12-16 16:44:21 +0100

    notesDirecteur

---
+++ Directeur.java
@@ -0,0 +1,12 @@
+
+public class Directeur extends Utilisateur {
+	
+	public Directeur (){
+		super();
+	}
+
+	public Directeur (String id,String prenom,String nom,String mdp){
+		super(id,prenom,nom,mdp);
+	}
+
+}
